a6a2f71916c9fcfdce532f672261f42616d089f6
wangxiaoyang <wangxiaoyang@kingsoft.com>
2016-09-17 17:49:27 +0800

    add send

---
+++ src/main/java/com/seansun/weixinmp/service/MessageService.java
@@ -0,0 +1,428 @@
+/**
+ * 
+ */
+package com.seansun.weixinmp.service;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.seansun.weixinmp.repository.WeixinRepository;
+import com.seansun.weixinmp.service.vo.ArticleMessage;
+import com.seansun.weixinmp.service.vo.AuthorizeUrlRequest;
+import com.seansun.weixinmp.service.vo.FilterObject;
+import com.seansun.weixinmp.service.vo.GroupMessageRequest;
+import com.seansun.weixinmp.service.vo.MediaMessage;
+import com.seansun.weixinmp.service.vo.OpenidListMessageRequest;
+import com.seansun.weixinmp.service.vo.OpenidMessageRequest;
+import com.seansun.weixinmp.service.vo.SendResponse;
+import com.seansun.weixinmp.service.vo.TextMessage;
+import com.seansun.weixinmp.service.vo.UploadMediaResponse;
+import com.seansun.weixinmp.service.vo.UploadNewsRequest;
+import com.seansun.weixinmp.service.vo.UploadNewsResponse;
+import com.seansun.weixinmp.service.vo.WeixinErrorResponse;
+import com.seansun.weixinmp.utils.ErrorCode;
+import com.seansun.weixinmp.utils.HttpUtils;
+
+/**
+ *
+ */
+@Service
+public class MessageService {
+	private static final Logger logger = LoggerFactory.getLogger(MessageService.class);
+	public static final String FLAG_ERROR = "{\"err";
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private WeixinRepository weixinRepository;
+	//预览接口:开发者可通过该接口发送消息给指定用户，在手机端查看消息的样式和排版。
+	//为了满足第三方平台开发者的需求，在保留对openID预览能力的同时，增加了对指定微信号发送预览的能力，但该能力每日调用次数有限制（100次），请勿滥用
+	public static final String URL_SENDOPENID = "https://api.weixin.qq.com/cgi-bin/message/mass/preview?access_token={0}";
+	public static final String URL_SENDOPENID_CUSTOM = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={0}";
+	public static final String URL_SENDGROUP = "https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token={0}";
+	public static final String URL_UPLOADMEDIA = "https://api.weixin.qq.com/cgi-bin/media/upload?access_token={0}&type={1}";
+	public static final String URL_UPLOADNEWS = "https://api.weixin.qq.com/cgi-bin/media/uploadnews?access_token={0}";
+	
+	//根据OpenID列表群发【订阅号不可用，服务号认证后可用】
+	public static final String URL_SENDOPENIDLIST = "https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token={0}";
+	public static final String URL_USERDEFINEDMENU = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token={0}";
+	
+	public SendResponse sendText2Card(String cardId, String content) {
+		
+		String openid = cardId; //查询db获取openid
+		
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_SENDOPENID, accessToken);
+		OpenidMessageRequest request = new OpenidMessageRequest();
+		request.setTouser(openid);
+		request.setMsgtype(OpenidMessageRequest.MSGTYPE_TEXT);
+		TextMessage textMessage = new TextMessage();
+		textMessage.setContent(content);
+		request.setText(textMessage);
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in sendText2Openid to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		return sendResponse;
+	}
+
+	public SendResponse sendText2Openid(String openid, String content) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_SENDOPENID, accessToken);
+		OpenidMessageRequest request = new OpenidMessageRequest();
+		request.setTouser(openid);
+		request.setMsgtype(OpenidMessageRequest.MSGTYPE_TEXT);
+		TextMessage textMessage = new TextMessage();
+		textMessage.setContent(content);
+		request.setText(textMessage);
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in sendText2Openid to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		return sendResponse;
+	}
+	
+	public SendResponse sendText2OpenidList(List<String> openids, String content) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_SENDOPENID, accessToken); //可能有问题 应该为： URL_SENDOPENIDLIST，待测试
+		OpenidListMessageRequest request = new OpenidListMessageRequest();
+		request.setTouser(openids);
+		request.setMsgtype(OpenidMessageRequest.MSGTYPE_TEXT);
+		TextMessage textMessage = new TextMessage();
+		textMessage.setContent(content);
+		request.setText(textMessage);
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in sendText2Openid to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		return sendResponse;
+	}
+
+	public SendResponse sendText2OpenidList(List<String> openids, String text, String redirectUrl, String reportId) {
+		StringBuilder content = new StringBuilder();
+		content.append(text);
+		AuthorizeUrlRequest request = new AuthorizeUrlRequest();
+		request.setAppid(weixinRepository.getAppid());
+		request.setState(reportId);
+		if (StringUtils.isEmpty(redirectUrl)) {
+			redirectUrl = "http://wx.xgsdk.com/showReport";
+		}
+		request.setRedirect_uri(redirectUrl);
+		content.append("\n").append(MessageFormat.format("&lt;a href=\"{0}\"&gt;点击阅读&lt;/a&gt;", request.getUrl()));
+		SendResponse sendResponse = this.sendText2OpenidList(openids, content.toString());
+		return sendResponse;
+	}
+
+	public SendResponse sendText2Openid(String openid, String text, String redirectUrl, String reportId) {
+		StringBuilder content = new StringBuilder();
+		content.append(text);
+		AuthorizeUrlRequest request = new AuthorizeUrlRequest();
+		request.setAppid(weixinRepository.getAppid());
+		request.setState(reportId);
+		if (StringUtils.isEmpty(redirectUrl)) {
+			redirectUrl = "http://wx.xgsdk.com/showReport";
+		}
+		request.setRedirect_uri(redirectUrl);
+		content.append("\n").append(MessageFormat.format("<a href=\"{0}\">点击阅读</a>", request.getUrl()));
+		SendResponse sendResponse = this.sendText2Openid(openid, content.toString());
+		return sendResponse;
+	}
+
+	public SendResponse sendText2Group(String groupId, String content) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_SENDGROUP, accessToken);
+		GroupMessageRequest request = new GroupMessageRequest();
+		request.setMsgtype(OpenidMessageRequest.MSGTYPE_TEXT);
+		TextMessage textMessage = new TextMessage();
+		textMessage.setContent(content);
+		request.setText(textMessage);
+		FilterObject filterObject = new FilterObject();
+		if (!StringUtils.equals("0", groupId)) {
+			filterObject.setIs_to_all(false);
+			filterObject.setGroup_id(groupId);
+			request.setFilter(filterObject);
+		}
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in sendText2Group to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		return sendResponse;
+	}
+
+	public SendResponse sendText2Group(String groupId, String text, String redirectUrl, String reportId) {
+		StringBuilder content = new StringBuilder();
+		content.append(text);
+		AuthorizeUrlRequest request = new AuthorizeUrlRequest();
+		request.setAppid(weixinRepository.getAppid());
+		request.setState(reportId);
+		if (StringUtils.isEmpty(redirectUrl)) {
+			redirectUrl = "http://wx.xgsdk.com/showReport";
+		}
+		request.setRedirect_uri(redirectUrl);
+		content.append("\n").append(MessageFormat.format("<a href=\"{0}\">点击阅读</a>", request.getUrl()));
+		SendResponse sendResponse = this.sendText2Group(groupId, content.toString());
+		return sendResponse;
+	}
+
+	public SendResponse uploadImage(String fileName, String mediaType) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_UPLOADMEDIA, accessToken, mediaType);
+		List<String> fileNames = new ArrayList<>();
+		fileNames.add(fileName);
+		String responseString = HttpUtils.doPostWeixin(url, fileName, null);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in uploadImage to the url:{0}, fileName:{1}, mediaType:{2}, response:{3}", url, fileName, mediaType, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		if (StringUtils.startsWith(responseString, FLAG_ERROR)) {
+			String msg = MessageFormat.format("error in uploadImage to the url:{0}, fileName:{1}, mediaType:{2}, response:{3}", url, fileName, mediaType, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_RETURN);
+			sendResponse.setMsg(msg);
+			sendResponse.setOriginString(responseString);
+			return sendResponse;
+		}
+		UploadMediaResponse response = JSON.parseObject(responseString, UploadMediaResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(ErrorCode.SUCCESS);
+		sendResponse.setMsg(response.getMedia_id());
+		sendResponse.setOriginString(responseString);
+		return sendResponse;
+	}
+
+	public SendResponse uploadNews(String thumbMedia_id, String author, String title, String contentSourceUrl, String content, String digest, String showCoverPic) {
+		// "thumb_media_id":"qI6_Ze_6PtV7svjolgs-rN6stStuHIjs9_DidOHaj0Q-mwvBelOXCFZiq2OsIU-p",
+		// "author":"xxx",
+		// "title":"Happy Day",
+		// "content_source_url":"www.qq.com",
+		// "content":"content",
+		// "digest":"digest",
+		// "show_cover_pic":"1"
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_UPLOADNEWS, accessToken);
+		UploadNewsRequest request = new UploadNewsRequest();
+		ArticleMessage message = new ArticleMessage();
+		request.addArticle(message);
+		message.setThumb_media_id(thumbMedia_id);
+		message.setAuthor(author);
+		message.setTitle(title);
+		message.setContent_source_url(contentSourceUrl);
+		message.setContent(content);
+		message.setDigest(digest);
+		message.setShow_cover_pic(showCoverPic);
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in uploadImage to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		if (StringUtils.startsWith(responseString, FLAG_ERROR)) {
+			String msg = MessageFormat.format("error in uploadImage to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_RETURN);
+			sendResponse.setMsg(msg);
+			sendResponse.setOriginString(responseString);
+			return sendResponse;
+		}
+		UploadNewsResponse response = JSON.parseObject(responseString, UploadNewsResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(ErrorCode.SUCCESS);
+		sendResponse.setMsg(response.getMedia_id());
+		sendResponse.setOriginString(responseString);
+		return sendResponse;
+	}
+
+	public SendResponse sendImage2Openid(String openid, String mediaId) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_SENDOPENID, accessToken);
+		OpenidMessageRequest request = new OpenidMessageRequest();
+		request.setTouser(openid);
+		request.setMsgtype(OpenidMessageRequest.MSGTYPE_IMAGE);
+		MediaMessage message = new MediaMessage();
+		message.setMedia_id(mediaId);
+		request.setImage(message);
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in sendText2Openid to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		return sendResponse;
+	}
+
+	public SendResponse sendNews2Openid(String openid, String mediaId) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_SENDOPENID, accessToken);
+		OpenidMessageRequest request = new OpenidMessageRequest();
+		request.setTouser(openid);
+		request.setMsgtype(OpenidMessageRequest.MSGTYPE_MPNEWS);
+		MediaMessage message = new MediaMessage();
+		message.setMedia_id(mediaId);
+		request.setMpnews(message);
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in sendNews2Openid to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		return sendResponse;
+	}
+
+	public SendResponse sendNew2Group(String groupId, String mediaId) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_SENDGROUP, accessToken);
+		GroupMessageRequest request = new GroupMessageRequest();
+		request.setMsgtype(OpenidMessageRequest.MSGTYPE_MPNEWS);
+		MediaMessage message = new MediaMessage();
+		message.setMedia_id(mediaId);
+		request.setMpnews(message);
+		FilterObject filterObject = new FilterObject();
+		if (!StringUtils.equals("0", groupId)) {
+			filterObject.setIs_to_all(false);
+			filterObject.setGroup_id(groupId);
+			request.setFilter(filterObject);
+		}
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in sendNew2Group to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		return sendResponse;
+	}
+	public SendResponse sendText2OpenidByCustom(String openid, String content) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_SENDOPENID_CUSTOM, accessToken);
+		OpenidMessageRequest request = new OpenidMessageRequest();
+		request.setTouser(openid);
+		request.setMsgtype(OpenidMessageRequest.MSGTYPE_TEXT);
+		TextMessage textMessage = new TextMessage();
+		textMessage.setContent(content);
+		request.setText(textMessage);
+
+		String body = JSON.toJSONString(request);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in sendText2OpenidByCustom to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		return sendResponse;
+	}
+
+	public SendResponse userdefinedMenu(String body) {
+		String accessToken = userService.getAccessToken();
+		String url = MessageFormat.format(URL_USERDEFINEDMENU, accessToken);
+		String responseString = HttpUtils.doPostJson(url, body);
+		if (StringUtils.isEmpty(responseString)) {
+			String msg = MessageFormat.format("error in userdefinedMenu to the url:{0}, body:{1}, response:{2}", url, body, responseString);
+			logger.error(msg);
+			SendResponse sendResponse = new SendResponse();
+			sendResponse.setCode(ErrorCode.ERR_CONNECT);
+			sendResponse.setMsg(msg);
+			return sendResponse;
+		}
+		WeixinErrorResponse response = JSON.parseObject(responseString, WeixinErrorResponse.class);
+		SendResponse sendResponse = new SendResponse();
+		sendResponse.setCode(response.getErrcode());
+		sendResponse.setMsg(response.getErrmsg());
+		sendResponse.setOriginString(responseString);
+		return sendResponse;
+	}
+}
---
+++ src/main/java/com/seansun/weixinmp/service/UserService.java
@@ -0,0 +1,208 @@
+/**
+ * 
+ */
+package com.seansun.weixinmp.service;
+
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.seansun.weixinmp.mapper.ReportMapper;
+import com.seansun.weixinmp.repository.WeixinRepository;
+import com.seansun.weixinmp.service.model.WechatPushUser;
+import com.seansun.weixinmp.service.vo.AccessToken;
+import com.seansun.weixinmp.service.vo.Group;
+import com.seansun.weixinmp.service.vo.GroupList;
+import com.seansun.weixinmp.service.vo.ParseCode2AccessTokenRequest;
+import com.seansun.weixinmp.service.vo.ParseCode2AccessTokenResponse;
+import com.seansun.weixinmp.service.vo.User;
+import com.seansun.weixinmp.service.vo.UserList;
+import com.seansun.weixinmp.service.vo.WeixinUser;
+import com.seansun.weixinmp.utils.HttpUtils;
+
+/**
+ *
+ */
+@Service
+public class UserService {
+	
+	@Autowired
+	ReportMapper reportMapper;
+
+	@Autowired
+	private WeixinRepository weixinRepository;
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	public static final String URL_GETACCESSTOKEN = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}";
+	public static final String URL_GETUSERLIST = "https://api.weixin.qq.com/cgi-bin/user/get?access_token={0}&next_openid={1}";
+	public static final String URL_GETGROUPLIST = "https://api.weixin.qq.com/cgi-bin/groups/get?access_token={0}";
+	public static final String URL_USERINFO = "https://api.weixin.qq.com/cgi-bin/user/info?access_token={0}&openid={1}&lang=zh_CN";
+	public static final String FLAG_ERROR = "{\"err";
+
+	private AccessToken accessToken;
+
+	public String getAccessToken() {
+		// if (accessToken != null && !accessToken.isExpired()) {
+		// return accessToken.getAccess_token();
+		// }
+		String appid = weixinRepository.getAppid();
+		String secret = weixinRepository.getSecret();
+		String url = MessageFormat.format(URL_GETACCESSTOKEN, appid, secret);
+		String responseString = HttpUtils.doGet(url);
+		if (StringUtils.isEmpty(responseString)) {
+			logger.error("The accessToken data is empty from the url:{}.", url);
+			return null;
+		}
+		if (StringUtils.startsWith(responseString, FLAG_ERROR)) {
+			logger.error("The accessToken data can not be found from the url:{}. The error is {}", url, responseString);
+			return null;
+		}
+		this.accessToken = JSON.parseObject(responseString, AccessToken.class);
+		return accessToken.getAccess_token();
+	}
+
+	/**
+	 * 获取单个微信用户
+	 * 
+	 * @param openid
+	 * @return
+	 */
+	public User getUser(String openid) {
+		String accessToken = this.getAccessToken();
+		String url = MessageFormat.format(URL_USERINFO, accessToken, openid);
+		String responseString = HttpUtils.doGet(url);
+		if (StringUtils.isEmpty(responseString)) {
+			logger.error("The user data is empty from the url:{}.", url);
+			return null;
+		}
+		if (StringUtils.startsWith(responseString, FLAG_ERROR)) {
+			logger.error("The user data can not be found from the url:{}. The error is {}", url, responseString);
+			return null;
+		}
+		User user = JSON.parseObject(responseString, User.class);
+		return user;
+	}
+
+	/**
+	 * 获取微信用户列表
+	 * 
+	 * @return
+	 */
+	public Map<String, User> getUserMap() {
+		Map<String, User> userMap = new HashMap<>();
+		String accessToken = this.getAccessToken();
+		String nextOpenid = "";
+		int total = Integer.MAX_VALUE;
+		int current = 0;
+
+		while (total > current) {
+			String url = MessageFormat.format(URL_GETUSERLIST, accessToken, nextOpenid);
+			String responseString = HttpUtils.doGet(url);
+			if (StringUtils.isEmpty(responseString)) {
+				logger.error("The openid data is empty from the url:{}.", url);
+				return userMap;
+			}
+			if (StringUtils.startsWith(responseString, FLAG_ERROR)) {
+				logger.error("The openid data can not be found from the url:{}. The error is {}", url, responseString);
+				return userMap;
+			}
+			UserList userList = JSON.parseObject(responseString, UserList.class);
+			nextOpenid = userList.getNext_openid();
+			total = userList.getTotal();
+			current += userList.getCount();
+			if (userList.getCount() == 0) {
+				return userMap;
+			}
+			List<String> openids = userList.getData().getOpenid();
+			for (String openid : openids) {
+				User user = this.getUser(openid);
+				if (user != null) {
+					userMap.put(openid, user);
+				}
+			}
+		}
+		return userMap;
+	}
+
+	/**
+	 * 获取微信分组
+	 * 
+	 * @return
+	 */
+	public Map<String, Group> getGroupMap() {
+		String accessToken = this.getAccessToken();
+		String url = MessageFormat.format(URL_GETGROUPLIST, accessToken);
+		String responseString = HttpUtils.doGet(url);
+		if (StringUtils.isEmpty(responseString)) {
+			logger.error("The group data is empty from the url:{}.", url);
+			return new HashMap<>();
+		}
+		if (StringUtils.startsWith(responseString, FLAG_ERROR)) {
+			logger.error("The group data can not be found from the url:{}. The error is {}", url, responseString);
+			return new HashMap<>();
+		}
+		GroupList groupList = JSON.parseObject(responseString, GroupList.class);
+		List<Group> groups = groupList.getGroups();
+		Map<String, Group> groupMap = new HashMap<>();
+		for (Group group : groups) {
+			groupMap.put(group.getId(), group);
+		}
+		return groupMap;
+	}
+
+	/**
+	 * 通过code换取网页授权access_token
+	 * 
+	 * @param code
+	 * @return
+	 */
+	public ParseCode2AccessTokenResponse parseCode2AccessToken(String code) {
+		ParseCode2AccessTokenRequest request = new ParseCode2AccessTokenRequest();
+		request.setAppid(weixinRepository.getAppid());
+		request.setSecret(weixinRepository.getSecret());
+		request.setCode(code);
+		String url = request.getUrl();
+		String responseString = HttpUtils.doGet(url);
+		if (StringUtils.isEmpty(responseString)) {
+			logger.error("The response is empty in the url:{}.", url);
+			return null;
+		}
+		if (StringUtils.startsWith(responseString, FLAG_ERROR)) {
+			logger.error("The response has some error in the url:{}. The error is {}", url, responseString);
+			return null;
+		}
+		ParseCode2AccessTokenResponse response = JSON.parseObject(responseString, ParseCode2AccessTokenResponse.class);
+		return response;
+	}
+
+	public WeixinUser getWeixinUser(String openid) {
+		WechatPushUser wechatPushUser = reportMapper.getUserByOpenId(openid);
+		if (wechatPushUser == null) {
+			User user = this.getUser(openid);
+			if (user != null) {
+				wechatPushUser = reportMapper.getUserByNickname(user.getNickname());
+				if (wechatPushUser != null) {
+					wechatPushUser.setOpenId(openid);
+					int result = reportMapper.updateUser(wechatPushUser);
+					logger.info("updateUser requestData={}, response={}", JSON.toJSONString(wechatPushUser), result);
+				}
+			}
+		}
+
+		if (wechatPushUser != null) {
+			WeixinUser weixinUser = new WeixinUser();
+			weixinUser.setOpenid(openid);
+			weixinUser.setRoleType(wechatPushUser.getType().toString());
+			weixinUser.setXgAppId(wechatPushUser.getXgAppId());
+			return weixinUser;
+		}
+		return null;
+	}
+}
